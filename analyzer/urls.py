46cb1ca80ee0aab1a74cb89fa0aafc6e1e7a496c
Chetandabbe <dabbechetan0@com>
2024-02-22 00:20:38 +0530

    Files added

---
+++ analyzer/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.upload_statement, name='upload_statement'),  # Define a URL pattern for the root path
+    path('upload/', views.upload_statement, name='upload'),
+    path('query_transactions/', views.query_transactions, name='query_transactions'),
+]
+
+
+
+
+    # path('query/', views.query_transactions, name='query_transactions'),
